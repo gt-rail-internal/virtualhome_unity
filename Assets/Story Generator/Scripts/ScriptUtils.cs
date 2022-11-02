e22fd47bddac44493149d3324fdb4ba3165e6e0c
Nima Jadali <nimajadalipour@gmail.com>
2022-11-02 03:32:04 -0400

    Update ScriptUtils.cs

--- Assets/Story Generator/Scripts/ScriptUtils.cs
+++ Assets/Story Generator/Scripts/ScriptUtils.cs
@@ -1,4 +1,4 @@
-﻿using StoryGenerator.Helpers;
+using StoryGenerator.Helpers;
 using StoryGenerator.Utilities;
 using StoryGenerator.RoomProperties;
 using System;
@@ -447,6 +447,17 @@ namespace StoryGenerator.Scripts
             }
             return result;
         }
+
+        // public static List<Sensor> FindAllSensors(Transform tsfm)
+        // {
+        //     List<Sensor> result = new List<Sensor>();
+
+        //     foreach (GameObject go in ScriptUtils.FindAllObjects(tsfm, t => t.GetComponent<Sensor>() != null && t.GetComponent<Sensor>().enabled))
+        //     {
+        //         result.Add(go.GetComponent<Sensor>());
+        //     }
+        //     return result;
+        // }
 
         public static List<GameObject> FindAllCharacters(Transform tsfm)
         {
